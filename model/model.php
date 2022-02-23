2e30a8546f8fdadcf1071e6cec3d0b04bacce31f
Wuwpan4ik <dimalim110@gmail.com>
2022-02-23 14:06:19 +0400

    'MVC'

--- model/model.php
+++ model/model.php
@@ -6,11 +6,6 @@
 			require_once 'connect.php';
 			$this->db = new Database();
 		}
-
-		public function deleteTask($id_task) {
-			$db->execute("DELETE FROM `tasks` WHERE id = '". $id_task ."'");
-			redirect();
-		}
 		
 		public function viewTasks() {
 			$result = $this->db->query("SELECT `description`, `status`, `id` FROM `tasks` WHERE user_id = '". $_SESSION['id'] ."'");
