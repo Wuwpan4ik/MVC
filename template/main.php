7cf503c561a05a81efd581d557a8897ac6b9abc2
Wuwpan4ik <dimalim110@gmail.com>
2022-02-28 20:19:41 +0400

    Revert "'EditGet'"
    This reverts commit 78093b6e351e81bc594b88e493fb612fdc7ccb40.

--- template/main.php
+++ template/main.php
@@ -4,17 +4,17 @@
 			<div class="task__title">Task list</div>
 			<div class="task__main">
 				<div class="task__form">
-					<form action="?method=AddTask" method=\'POST\'>
+					<form action="?option=ManageTask&method=AddTask" method=\'POST\'>
 						<div class="form__block">
 							<input class=\'form__input\' name=\'task__text\' type="text" placeholder=\'Enter text...\'>
 							<button type="submit" name=\'AddTask\' class="form__btn form__btn-black">Add task</button>
 						</div>
 					</form>
 					<div class="form__block">
-						<form action="?method=DeleteAllTask" method=\'POST\'>
+						<form action="?option=ManageTask&method=DeleteAllTask" method=\'POST\'>
 							<button type="submit" name=\'DeleteAll\' class="form__btn btn ">Remove all</button>
 						</form>
-						<form action="?method=ReadyAllTask" method=\'POST\'>
+						<form action="?option=ManageTask&method=ReadyAllTask" method=\'POST\'>
 							<button type="submit" name=\'ReadyAll\' class="form__btn btn ">Ready all</button>
 					</form>
 					</div>
@@ -35,10 +35,10 @@
 					<div class=\"task__item-main\">
 						<div class=\"task__item-info\">
 							<div class=\"task__item-info-text\">{$desc}</div>
-							<form style='display: inline-block;' action=\"?method={$statusForText}Task&task_id={$id}\" method='POST'>
+							<form style='display: inline-block;' action=\"?option=ManageTask&method={$statusForText}Task&task_id={$id}\" method='POST'>
 								<button name='{$statusForText}{$id}' class=\"form__btn\">{$statusForText}</button>
 							</form>
-							<form style='display: inline-block;' action=\"?method=DeleteTask&task_id={$id}\" method='POST'>
+							<form style='display: inline-block;' action=\"?option=ManageTask&method=DeleteTask&task_id={$id}\" method='POST'>
 								<button name='Delete{$id}' class=\"form__btn\">Delete</inp>
 							</form>
 						</div>
