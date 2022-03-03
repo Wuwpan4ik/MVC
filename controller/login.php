167cdbf5215a0425cbc7ccda319b961a9ea0df98
Wuwpan4ik <dimalim110@gmail.com>
2022-03-03 15:31:19 +0400

    'LatestVersion'

--- controller/login.php
+++ controller/login.php
@@ -5,35 +5,35 @@
 			$password = $_POST['password'];
 			if(isset($_POST['register'])) {
 				if (!empty($_POST['login']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])) {
-						if ($_POST['password'] == $_POST['confirm_password']) {
-							if  (count($this->db->db->query("SELECT * FROM `users` WHERE `login` ='" . $login . "'")) == 0) {
-								$text = "INSERT INTO `users` (`login`, `password`) VALUES ('". $login ."', '". $password ."')";
-								$this->db->db->execute($text);
-								$_SESSION['id'] = $this->db->db->query("SELECT `id` FROM `users` WHERE `login` ='" . $login . "'")[0]['id'];
-								$this->redirect();
-							} else {
-								$_SESSION['message'] = 'Данный логин занят';
-							}
-						} else {
-							$_SESSION['message'] = 'Пароли разные';
-						};
-					} else {
-						$_SESSION['message'] = 'Все поля должны быть заполнены';
-					};
-				};
-				if(isset($_POST['auth'])) {
-					if (!empty($_POST['login']) && !empty($_POST['password'])) {
-						if (count($this->db->db->query("SELECT * FROM `users` WHERE `login` =" . '\'' . $login . '\'' . " AND `password` =" . '\'' .$password .'\'')) > 0) {
-							$message = 'Вы успешно авторизовались!';
+					if ($_POST['password'] == $_POST['confirm_password']) {
+						if  (count($this->db->db->query("SELECT * FROM `users` WHERE `login` ='" . $login . "'")) == 0) {
+							$text = "INSERT INTO `users` (`login`, `password`) VALUES ('". $login ."', '". $password ."')";
+							$this->db->db->execute($text);
 							$_SESSION['id'] = $this->db->db->query("SELECT `id` FROM `users` WHERE `login` ='" . $login . "'")[0]['id'];
 							$this->redirect();
-						} else { 
-							$_SESSION['message'] = 'Неверный пароль';
+						} else {
+							$_SESSION['message'] = 'Данный логин занят';
 						}
 					} else {
-						$_SESSION['message'] = 'Все поля должны быть заполнены';
+						$_SESSION['message'] = 'Пароли разные';
 					};
-				}
+				} else {
+					$_SESSION['message'] = 'Все поля должны быть заполнены';
+				};
+			};
+			if(isset($_POST['auth'])) {
+				if (!empty($_POST['login']) && !empty($_POST['password'])) {
+					if (count($this->db->db->query("SELECT * FROM `users` WHERE `login` =" . '\'' . $login . '\'' . " AND `password` =" . '\'' .$password .'\'')) > 0) {
+						$message = 'Вы успешно авторизовались!';
+						$_SESSION['id'] = $this->db->db->query("SELECT `id` FROM `users` WHERE `login` ='" . $login . "'")[0]['id'];
+						$this->redirect();
+					} else { 
+						$_SESSION['message'] = 'Неверный пароль';
+					}
+				} else {
+					$_SESSION['message'] = 'Все поля должны быть заполнены';
+				};
+			}
 		}
 		public function get_content() {
 		}
